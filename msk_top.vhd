f7503c3074f08b5c53f0abd1246ab0c4f24c95b5
Matthew Wishek <matthew@wishek.com>
2024-05-06 13:57:13 -0600

    Updates to MSK Top-level for Massey modulator

--- msk_top.vhd
+++ msk_top.vhd
@@ -15,10 +15,10 @@ ENTITY msk_top IS
 		clk 			: IN  std_logic;
 		init 			: IN  std_logic;
 
-		tx_freq_word_ft : IN  std_logic_vector(NCO_W -1 DOWNTO 0);
-		tx_freq_word_fc : IN  std_logic_vector(NCO_W -1 DOWNTO 0);
-		rx_freq_word_f1 : IN  std_logic_vector(NCO_W -1 DOWNTO 0);
-		rx_freq_word_f2 : IN  std_logic_vector(NCO_W -1 DOWNTO 0);
+		freq_word_ft 	: IN  std_logic_vector(NCO_W -1 DOWNTO 0);
+		freq_word_fc 	: IN  std_logic_vector(NCO_W -1 DOWNTO 0);
+		freq_word_f1 	: IN  std_logic_vector(NCO_W -1 DOWNTO 0);
+		freq_word_f2 	: IN  std_logic_vector(NCO_W -1 DOWNTO 0);
 
 		tx_req 			: OUT std_logic;
 		tx_data 		: IN  std_logic_vector(1 DOWNTO 0);
@@ -27,7 +27,7 @@ ENTITY msk_top IS
 		--rx_samples 		: IN  std_logic_vector(SAMPLE_W -1 DOWNTO 0);
 
 		rx_valid 		: OUT std_logic;
-		rx_data 		: OUT std_logic_vector(1 DOWNTO 0)
+		rx_data 		: OUT std_logic
 	);
 END ENTITY msk_top;
 
@@ -38,10 +38,44 @@ ARCHITECTURE struct OF msk_top IS
 	SIGNAL tx_req_int 		: std_logic;
 	SIGNAL tclk 			: std_logic;
 
+	SIGNAL tx_req_d 		: std_logic;
+	SIGNAL rx_data_int 		: std_logic;
+	SIGNAL rx_valid_int 	: std_logic;
+	SIGNAL sent_data 		: std_logic;
+	SIGNAL received_data 	: std_logic;
+	SIGNAL sent_data_pipe 	: std_logic_vector(0 TO 3);
+
+	SIGNAL bit_error 		: std_logic;
+
 BEGIN 
 
 	tx_req <= tx_req_int;
 	tx_samples <= txrx_samples;
+
+	rx_data <= rx_data_int;
+	rx_valid <= rx_valid_int;
+
+	data_check : PROCESS (clk)
+	BEGIN
+		IF clk'EVENT AND clk = '1' THEN
+
+			tx_req_d <= tx_req_int;
+
+			IF tx_req_d = '1' THEN
+				sent_data_pipe <= tx_data(0) & sent_data_pipe(0 TO 2);
+			END IF;
+
+			IF rx_valid_int = '1' THEN
+				sent_data <= sent_data_pipe(2);
+				received_data <= rx_data_int;
+			END IF;
+
+			bit_error <= sent_data XOR received_data;
+
+			ASSERT sent_data = received_data REPORT "Data Mismatch" SEVERITY WARNING;
+
+		END IF;
+	END PROCESS data_check;
 
 	u_mod : ENTITY work.msk_modulator(rtl)
 		GENERIC MAP (
@@ -54,12 +88,13 @@ BEGIN
 			clk 			=> clk,
 			init 			=> init,
 
-			tclk 			=> tclk,
+			tclk_out		=> tclk,
 
-			mod_freq_word 	=> tx_freq_word_ft,
-			car_freq_word	=> tx_freq_word_fc,
+			freq_word_tclk 	=> freq_word_ft,
+			freq_word_f1 	=> freq_word_f1,
+			freq_word_f2	=> freq_word_f2,
 
-			tx_data 		=> tx_data,
+			tx_data 		=> tx_data(0),
 			tx_req 			=> tx_req_int,
 
 			tx_samples	 	=> txrx_samples		
@@ -79,13 +114,13 @@ BEGIN
 
 			tclk 			=> tclk,
 	
-			rx_freq_word_f1 => rx_freq_word_f1,
-			rx_freq_word_f2	=> rx_freq_word_f2,
+			rx_freq_word_f1 => freq_word_f1,
+			rx_freq_word_f2	=> freq_word_f2,
 	
 			rx_samples 		=> txrx_samples,
 	
-			rx_data 		=> rx_data,
-			rx_valid 		=> rx_valid
+			rx_data 		=> rx_data_int,
+			rx_valid 		=> rx_valid_int
 		);
 
 
