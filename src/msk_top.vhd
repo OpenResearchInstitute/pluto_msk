925380a031f3ad53306bba68d6486c9a59c72806
Matthew Wishek <matthew@wishek.com>
2024-05-16 20:26:34 -0600

    Update configuration inputs, add loopback_ena signal and mux.

--- src/msk_top.vhd
+++ src/msk_top.vhd
@@ -9,7 +9,8 @@ ENTITY msk_top IS
 		NCO_W 			: NATURAL := 32;
 		PHASE_W 		: NATURAL := 10;
 		SINUSOID_W 		: NATURAL := 12;
-		SAMPLE_W 		: NATURAL := 12
+		SAMPLE_W 		: NATURAL := 12;
+		GAIN_W 			: NATURAL := 16
 	);
 	PORT (
 		clk 			: IN  std_logic;
@@ -20,11 +21,19 @@ ENTITY msk_top IS
 		freq_word_f1 	: IN  std_logic_vector(NCO_W -1 DOWNTO 0);
 		freq_word_f2 	: IN  std_logic_vector(NCO_W -1 DOWNTO 0);
 
+		lpf_p_gain 		: IN  std_logic_vector(GAIN_W -1 DOWNTO 0);
+		lpf_i_gain 		: IN  std_logic_vector(GAIN_W -1 DOWNTO 0);
+		lpf_freeze 		: IN  std_logic;
+		lpf_zero   		: IN  std_logic;
+		lpf_alpha  		: IN  std_logic_vector(GAIN_W -1 DOWNTO 0);
+
+		loopback_ena 	: IN  std_logic;
+
 		tx_req 			: OUT std_logic;
 		tx_data 		: IN  std_logic_vector(1 DOWNTO 0);
 
 		tx_samples 		: OUT std_logic_vector(SAMPLE_W -1 DOWNTO 0);
-		--rx_samples 		: IN  std_logic_vector(SAMPLE_W -1 DOWNTO 0);
+		rx_samples 		: IN  std_logic_vector(SAMPLE_W -1 DOWNTO 0);
 
 		rx_valid 		: OUT std_logic;
 		rx_data 		: OUT std_logic
@@ -34,7 +43,8 @@ END ENTITY msk_top;
 
 ARCHITECTURE struct OF msk_top IS 
 
-	SIGNAL txrx_samples		: std_logic_vector(SAMPLE_W -1 DOWNTO 0);
+	SIGNAL tx_samples_int	: std_logic_vector(SAMPLE_W -1 DOWNTO 0);
+	SIGNAL rx_samples_mux	: std_logic_vector(SAMPLE_W -1 DOWNTO 0);
 	SIGNAL tx_req_int 		: std_logic;
 	SIGNAL tclk 			: std_logic;
 
@@ -49,11 +59,13 @@ ARCHITECTURE struct OF msk_top IS
 
 BEGIN 
 
-	tx_req <= tx_req_int;
-	tx_samples <= txrx_samples;
+	tx_req 		<= tx_req_int;
+	tx_samples 	<= tx_samples_int;
 
-	rx_data <= rx_data_int;
-	rx_valid <= rx_valid_int;
+	rx_data 	<= rx_data_int;
+	rx_valid 	<= rx_valid_int;
+
+	rx_samples_mux <= tx_samples_int WHEN loopback_ena = '1' ELSE rx_samples;
 
 	data_check : PROCESS (clk)
 	BEGIN
@@ -66,7 +78,7 @@ BEGIN
 			END IF;
 
 			IF rx_valid_int = '1' THEN
-				sent_data <= sent_data_pipe(3);
+				sent_data <= sent_data_pipe(2);
 				received_data <= rx_data_int;
 			END IF;
 
@@ -95,7 +107,7 @@ BEGIN
 			tx_data 		=> tx_data(0),
 			tx_req 			=> tx_req_int,
 
-			tx_samples	 	=> txrx_samples		
+			tx_samples	 	=> tx_samples_int		
 		);
 
 
@@ -113,8 +125,14 @@ BEGIN
 			rx_freq_word_f1 => freq_word_f1,
 			rx_freq_word_f2	=> freq_word_f2,
 	
-			rx_samples 		=> txrx_samples,
-	
+			lpf_p_gain 		=> lpf_p_gain,
+			lpf_i_gain 		=> lpf_i_gain,
+			lpf_freeze 	 	=> lpf_freeze,
+			lpf_zero 		=> lpf_zero,
+			lpf_alpha 		=> lpf_alpha,
+
+			rx_samples 		=> rx_samples_mux,
+
 			rx_data 		=> rx_data_int,
 			rx_valid 		=> rx_valid_int
 		);
