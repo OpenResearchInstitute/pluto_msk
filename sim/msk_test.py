bc1984b2206f5e3e53fe56d1d22540f3dbe3fc26
Matthew Wishek <matthew@wishek.com>
2024-05-20 15:18:00 -0600

    Updated simulation to use AXI-S for data and AXI for configuration

--- sim/msk_test.py
+++ sim/msk_test.py
@@ -2,7 +2,7 @@
 
 import cocotb
 from cocotb.triggers import Timer, RisingEdge
-from cocotb.clock import Clock
+from cocotb.clock    import Clock
 from cocotb.utils    import get_sim_time
 
 import random
@@ -44,17 +44,214 @@ def fftPlot(sig, dt=None, plot=True):
 
     return sigFFTPos, freqAxisPos
 
+class axis_bus:
+
+    def __init__(self, dut):
+
+        self.dut       = dut
+        self.aclk_freq = 100e6
+
+        self.aclk_per  = int(1/self.aclk_freq * 1e9)
+        self.aclk_per_units = "ns"
+
+        self.aclk           = dut.s_axis_aclk
+        self.aresetn        = dut.s_axis_aresetn
+        self.tvalid         = dut.s_axis_tvalid
+        self.tready         = dut.s_axis_tready
+        self.tdata          = dut.s_axis_tdata
+
+        self.aresetn.value  = 1
+        self.tvalid.value   = 0 
+        self.tdata.value    = 0 
+
+    async def init(self):
+        await self._clock();
+        await self._reset();
+
+    async def _clock(self):
+
+        self.dut._log.info("starting aclk with period %d %s" % (self.aclk_per, self.aclk_per_units))
+        await cocotb.start(Clock(self.aclk, self.aclk_per, units=self.aclk_per_units).start())
+
+    async def _reset(self):
+
+        self.aresetn.value = 0
+
+        await Timer(50, "ns")
+
+        await RisingEdge(self.aclk)
+
+        self.aresetn.value = 1
+
+    async def send(self, data):
+
+        await RisingEdge(self.aclk)
+
+        self.dut.s_axis_tvalid.value = 1
+        self.tdata.value = data
+
+        while self.tready.value == 0:
+            await RisingEdge(self.aclk)
+
+        self.tvalid = 0
+
+
+class axi_bus:
+
+    def __init__(self, dut):
+
+        self.dut       = dut
+        self.aclk_freq = 100e6
+
+        self.aclk_per  = int(1/self.aclk_freq * 1e9)
+        self.aclk_per_units = "ns"
+
+        self.aclk      = dut.s_axi_aclk
+        self.aresetn   = dut.s_axi_aresetn
+
+        self.awaddr    = dut.s_axi_awaddr 
+        self.awvalid   = dut.s_axi_awvalid
+        self.awready   = dut.s_axi_awready
+
+        self.wdata     = dut.s_axi_wdata  
+        self.wstrb     = dut.s_axi_wstrb  
+        self.wvalid    = dut.s_axi_wvalid 
+        self.wready    = dut.s_axi_wready 
+
+        self.bready    = dut.s_axi_bready 
+
+        self.araddr    = dut.s_axi_araddr 
+        self.arvalid   = dut.s_axi_arvalid
+        self.arready   = dut.s_axi_arready
+
+        self.rready    = dut.s_axi_rready 
+        self.rdata     = dut.s_axi_rdata  
+        self.rresp     = dut.s_axi_rresp  
+        self.rvalid    = dut.s_axi_rvalid 
+
+        self.bresp     = dut.s_axi_bresp  
+        self.bvalid    = dut.s_axi_bvalid 
+
+
+        self.awaddr.value = 0
+        self.awvalid.value = 0
+
+        self.wdata.value = 0
+        self.wstrb.value = 0
+        self.wvalid.value = 0
+
+        self.bready.value = 0
+
+        self.araddr.value = 0
+        self.arvalid.value = 0
+
+        self.rready.value = 0
+
+
+    async def init(self):
+        await self._clock();
+        await self._reset();
+
+    async def _clock(self):
+
+        self.dut._log.info("starting aclk with period %d %s" % (self.aclk_per, self.aclk_per_units))
+        await cocotb.start(Clock(self.aclk, self.aclk_per, units=self.aclk_per_units).start())
+
+    async def _reset(self):
+
+        self.aresetn.value = 0
+
+        await Timer(50, "ns")
+
+        await RisingEdge(self.aclk)
+
+        self.aresetn.value = 1
+
+
+    async def read(self, addr):
+
+        await RisingEdge(self.aclk)
+
+        self.arvalid.value = 1
+        self.araddr.value = addr
+
+        self.rready.value = 1
+
+        await RisingEdge(self.aclk)
+
+        while self.rvalid.value == 0 or self.arready.value == 0:
+            await RisingEdge(self.aclk)
+
+        self.arvalid = 0
+
+        return self.rdata.value.integer
+
+
+    async def write(self, addr, data):
+
+        await RisingEdge(self.aclk)
+
+        self.awvalid.value = 1
+        self.awaddr.value = addr
+
+        self.wstrb.value = 15
+        self.wdata.value = data
+        self.wvalid.value = 1
+
+        self.bready.value = 1
+
+        await RisingEdge(self.aclk)
+
+        while self.awready == 0:
+            await RisingEdge(self.aclk)
+
+        while self.bvalid.value == 0:
+            await RisingEdge(self.aclk)
+
+        self.awvalid.value = 0
+
+class msk:
+
+    def __init__(self, dut, clk, tx_sample_bus):
+
+        self.dut = dut
+        self.sim_run = False
+
+        self.tx_sample_bus = tx_sample_bus 
+        self.clk = clk
+
+        self.tx_samples = []
+        self.time = []
+
+    async def tx_sample_capture(self):
+
+        self.dut._log.info("tx sample capture - waiting for start...")
+
+        while self.sim_run == False:
+            await RisingEdge(self.clk)
+
+        self.dut._log.info("tx sample capture - starting...")
+
+        while self.sim_run:
+            self.tx_samples.append(self.tx_sample_bus.value.to_signed())
+            self.time.append(get_sim_time("us"))
+            await RisingEdge(self.clk)
+
+        self.dut._log.info("...tx sample capture - done")
+
+
 @cocotb.test()
 async def msk_test_1(dut):
 
     tx_samples = []
     tx_time = []
 
-    await cocotb.start(Clock(dut.clk, 10, units="ns").start())
-
     bitrate = 1e6
     freq_if = 10e6
-    sample_rate = 160e6
+    sample_rate = 3*61.46e6
+    sample_per = int(1/sample_rate * 1e9)
+
+    await cocotb.start(Clock(dut.clk, sample_per, units="ns").start())
 
     f1 =  9e6
     f2 = 11e6
@@ -63,52 +260,54 @@ async def msk_test_1(dut):
 
     await RisingEdge(dut.clk)
 
-    dut.clk.value = 0
-    dut.init.value = 1
-    dut.freq_word_ft.value = int(bitrate / sample_rate * 2.0**32)
-    dut.freq_word_f1.value = int(f1 / sample_rate * 2.0**32);
-    dut.freq_word_f2.value = int(f2 / sample_rate * 2.0**32);
-    dut.lpf_p_gain.value = 50
-    dut.lpf_i_gain.value = 20
-    dut.lpf_freeze.value = 0
-    dut.lpf_zero.value = 0
-    dut.lpf_alpha.value = 2
+    axi  = axi_bus(dut)
+    axis = axis_bus(dut)
 
-    dut.loopback_ena.value = 1
-    dut.ptt.value = 0
+    await axi.init()
+    await axis.init()
 
-    dut.tx_data.value = 0
+    await axi.write( 0, 1)                                         # tun on init
+
+    await axi.write( 8, 1)                                         # loopback
+    await axi.write(12, int(bitrate / sample_rate * 2.0**32))      # bit rate frequency word
+    await axi.write(16, int(f1 / sample_rate * 2.0**32))           # F1 frequency word
+    await axi.write(20, int(f2 / sample_rate * 2.0**32))           # F2 frequency word
+    await axi.write(24, (50 << 16) + 20)                             # p-gain / i-gain
+    await axi.write(28, (2 << 16))                                   # low-pass filter alpha
 
     await Timer(100, units="ns")
 
     await RisingEdge(dut.clk)
 
-    dut.init.value = 1
+    await axi.write(0, 0)                                         # turn off init
 
     await RisingEdge(dut.clk)
 
-    dut.init.value = 0
+    await Timer(105, "us")
+    await axi.write(4, 1)
 
-    await RisingEdge(dut.clk)
+    msksim = msk(dut, dut.clk, dut.tx_samples)
 
-    sim_time = 0
+    await cocotb.start(msksim.tx_sample_capture())
+
+    sim_time = get_sim_time("us")
+    sim_start = sim_time
 
     dut._log.info("starting...")
 
-    while sim_time < 30000:
+    msksim.sim_run = True
 
-        if sim_time > 105:
-            dut.ptt.value = 1
+    while sim_time < sim_start + 30000:
 
-        if dut.tx_req.value == 1:
-            dut.tx_data.value = random.randrange(1000) % 2
-
-        await RisingEdge(dut.clk)
-
-        tx_samples.append(dut.tx_samples.value.signed_integer)
-        tx_time.append(sim_time)
-
+        await axis.send(random.randrange(1000) % 256)
         sim_time = get_sim_time("us")
+
+    msksim.sim_run = False
+
+    await RisingEdge(dut.clk)
+
+    tx_time        = msksim.time
+    tx_samples     = msksim.tx_samples
 
     tx_samples_arr = np.asarray(tx_samples)
     tx_samples_2   = tx_samples_arr * tx_samples_arr
