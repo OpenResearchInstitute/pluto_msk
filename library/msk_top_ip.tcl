d9dccc714479ae0cf61298cbf4a02596de1e9b1e
Matthew Wishek <matthew@wishek.com>
2024-07-29 11:22:49 -0600

    Add RDL files in msk_top_ip.tcl

--- library/msk_top_ip.tcl
+++ library/msk_top_ip.tcl
@@ -7,6 +7,10 @@ source $ad_hdl_dir/library/scripts/adi_ip_xilinx.tcl
 adi_ip_create msk_top
 #set_property FILE_TYPE {VHDL 2008} [get_files $ad_hdl_dir/library/msk_top/src/*.vhd]
 adi_ip_files msk_top [list \
+  "../rdl/vhdl/desyrdl/pkg_desyrdl_common.vhd" \
+  "../rdl/vhdl/msk_top_regs/msk_top_regs_decoder_axi4l.vhd" \
+  "../rdl/vhdl/msk_top_regs/pkg_msk_top_regs.vhd" \
+  "../rdl/vhdl/msk_top_regs/msk_top_regs.vhd" \
   "../msk_demodulator/src/costas_loop.vhd" \
   "../pi_controller/src/pi_controller.vhd" \
   "../msk_demodulator/src/msk_demodulator.vhd" \
