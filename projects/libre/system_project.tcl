391a18a4b274fa2aa7fe56ad38f65a4eec121c83
Abraxas3d <w5nyv@yahoo.com>
2025-12-01 18:49:09 -0800

    Fix system_project.tcl path to adi_env.tcl

--- projects/libre/system_project.tcl
+++ projects/libre/system_project.tcl
@@ -1,4 +1,4 @@
-source ../../scripts/adi_env.tcl
+source ../../hdl/scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
@@ -10,5 +10,6 @@ adi_project_files libre [list \
   "$ad_hdl_dir/library/common/ad_iobuf.v"]
 
 set_property is_enabled false [get_files  *system_sys_ps7_0.xdc]
+
 adi_project_run libre
 source $ad_hdl_dir/library/axi_ad9361/axi_ad9361_delay.tcl
