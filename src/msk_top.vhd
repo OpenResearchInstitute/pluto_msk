632235aa090a5a94ba5198ac250992474f56e682
Abraxas3d <w5nyv@yahoo.com>
2025-12-07 21:49:52 -0800

    went ahead and added a missing output port for soft data from demodulator.

--- src/msk_top.vhd
+++ src/msk_top.vhd
@@ -616,7 +616,8 @@ BEGIN
             rx_bit          => rx_bit_corr,
             rx_bit_valid    => rx_bit_valid,
             --s_axis_soft_tdata => (OTHERS => '0'),  -- tied to zero for hard decisions
-            s_axis_soft_tdata => rx_data_soft,  -- Connect rx_data_soft for soft decisions            
+            s_axis_soft_tdata => rx_data_soft,  -- Connect rx_data_soft for soft decisions
+            m_axis_soft_tdata => OPEN,
 
             m_axis_tdata    => sync_det_tdata,
             m_axis_tvalid   => sync_det_tvalid,
