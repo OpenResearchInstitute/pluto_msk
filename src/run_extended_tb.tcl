a82a364b76f59490d4b7e73fab326e6c1b37b85d
Abraxas3d <w5nyv@yahoo.com>
2026-01-07 14:34:01 -0800

    Remove test benches and test scripts from the source directory. Retired files include run_extended_tb.tcl, run_soft_tb.tcl, tb_viterbi_extended.vhd, and tb_viterbi_soft.vhd.

--- src/run_extended_tb.tcl
+++
@@ -1,6 +0,0 @@
-create_project -force tb_extended ./tb_extended -part xc7z020clg400-2
-add_files {viterbi_decoder_k7_simple.vhd viterbi_decoder_k7_soft.vhd tb_viterbi_extended.vhd}
-set_property top tb_viterbi_extended [get_filesets sim_1]
-set_property target_language VHDL [current_project]
-launch_simulation
-run all
